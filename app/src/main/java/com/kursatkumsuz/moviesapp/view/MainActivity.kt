c36f57813887cbf1f5173b3f42440e3853317948
DrHistory <kursatkumsuz@gmail.com>
2022-08-08 15:37:22 +0300

    delete unused imports

--- app/src/main/java/com/kursatkumsuz/moviesapp/view/MainActivity.kt
+++ app/src/main/java/com/kursatkumsuz/moviesapp/view/MainActivity.kt
@@ -1,21 +1,9 @@
 package com.kursatkumsuz.moviesapp.view
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SearchView
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.kursatkumsuz.moviesapp.adapter.RecyclerViewAdapter
 import com.kursatkumsuz.moviesapp.databinding.ActivityMainBinding
-import com.kursatkumsuz.moviesapp.model.MovieModel
-import com.kursatkumsuz.moviesapp.service.MovieAPI
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
-import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
