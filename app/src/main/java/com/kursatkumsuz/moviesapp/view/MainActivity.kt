f97c2ef980a0ff89f1e2065ee91f353b2b492932
DrHistory <kursatkumsuz@gmail.com>
2022-08-08 15:36:13 +0300

    add FragmentContainerView and delete functions

--- app/src/main/java/com/kursatkumsuz/moviesapp/view/MainActivity.kt
+++ app/src/main/java/com/kursatkumsuz/moviesapp/view/MainActivity.kt
@@ -19,84 +19,12 @@ import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val BASE_URL = "https://gist.githubusercontent.com/"
-    private lateinit var adapter : RecyclerViewAdapter
-    private lateinit var movieList : ArrayList<MovieModel>
-    private lateinit var displayedList : ArrayList<MovieModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        displayedList = ArrayList()
-        // Functions
-        loadMovie()
-        searchView()
-        //RecyclerView
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-
     }
 
-    fun loadMovie() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val service = retrofit.create(MovieAPI::class.java)
-        val call = service.getMovie()
-
-        call.enqueue(object : Callback<List<MovieModel>> {
-            override fun onResponse(
-                call: Call<List<MovieModel>>,
-                response: Response<List<MovieModel>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        movieList = ArrayList(it)
-                        movieList.let { list ->
-                            displayedList.addAll(list)
-                            adapter = RecyclerViewAdapter(displayedList)
-                            binding.recyclerView.adapter = adapter
-                        }
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<MovieModel>>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
-
-    }
-
-    private fun searchView()
-    {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                return true
-            }
-            @SuppressLint("NotifyDataSetChanged")
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if(newText!!.isNotEmpty())
-                {
-                    displayedList.clear()
-                    val search = newText.lowercase(Locale.getDefault())
-                    movieList.forEach {
-                        if(it.title.lowercase(Locale.getDefault()).contains(search)) {
-                            displayedList.add(it)
-                        }
-                    }
-                    binding.recyclerView.adapter!!.notifyDataSetChanged()
-                }  else
-                {
-                    displayedList.clear()
-                    loadMovie()
-                    binding.recyclerView.adapter!!.notifyDataSetChanged()
-                }
-                return true
-            }
-        })
-    }
 }
