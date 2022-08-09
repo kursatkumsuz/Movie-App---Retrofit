5ba2900348c802eec9db4c615d02bba8dbb03c0b
DrHistory <kursatkumsuz@gmail.com>
2022-08-09 14:45:11 +0300

    add action to navigate

--- app/src/main/java/com/kursatkumsuz/moviesapp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/kursatkumsuz/moviesapp/adapter/RecyclerViewAdapter.kt
@@ -1,31 +1,29 @@
 package com.kursatkumsuz.moviesapp.adapter
 
-import android.app.Activity
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import com.kursatkumsuz.moviesapp.Singleton
 import com.kursatkumsuz.moviesapp.databinding.RecyclerRowBinding
+import com.kursatkumsuz.moviesapp.fragment.FeedFragmentDirections
 import com.kursatkumsuz.moviesapp.model.MovieModel
-import com.kursatkumsuz.moviesapp.view.DetailActivity
 
-class RecyclerViewAdapter(val movieList : ArrayList<MovieModel>) : RecyclerView.Adapter<RecyclerViewAdapter.MovieHolder>() {
+class RecyclerViewAdapter(val movieList: List<MovieModel>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MovieHolder>() {
     class MovieHolder(val binding: RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
-       val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MovieHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieHolder, position: Int) {
 
-        val animation  = AnimationUtils.loadAnimation(holder.itemView.context , android.R.anim.slide_in_left)
+        val animation = AnimationUtils.loadAnimation(holder.itemView.context, android.R.anim.slide_in_left)
         holder.itemView.startAnimation(animation)
 
         holder.binding.rowTextView.text = movieList[position].title
@@ -35,15 +33,13 @@ class RecyclerViewAdapter(val movieList : ArrayList<MovieModel>) : RecyclerView.
             crossfade(true)
         }
         holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView.context , DetailActivity::class.java)
-            Singleton.chosenMovie = movieList[position]
-            holder.itemView.context.startActivity(intent)
-
+            val action = FeedFragmentDirections.actionFeedFragmentToDetailFragment(key = movieList[position].primaryKey)
+            Navigation.findNavController(it).navigate(action)
         }
     }
 
     override fun getItemCount(): Int {
-       return movieList.size
+        return movieList.size
     }
 
 
