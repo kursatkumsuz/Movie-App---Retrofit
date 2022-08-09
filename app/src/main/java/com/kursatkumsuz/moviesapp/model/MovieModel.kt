aaaea0e25dc843cc95ca464a6de22c3a510f9f0a
DrHistory <kursatkumsuz@gmail.com>
2022-08-09 14:48:01 +0300

    add ColumnInfo

--- app/src/main/java/com/kursatkumsuz/moviesapp/model/MovieModel.kt
+++ app/src/main/java/com/kursatkumsuz/moviesapp/model/MovieModel.kt
@@ -1,18 +1,35 @@
 package com.kursatkumsuz.moviesapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+
+@Entity
 data class MovieModel(
-
+    @ColumnInfo(name = "name")
     @SerializedName("Title")
-    val title : String,
-    @SerializedName("Images")
-    val image : List<String>,
-    @SerializedName("imdbRating")
-    val rate : String,
-    @SerializedName("Plot")
-    val plot : String,
-    @SerializedName("Type")
-    val type : String
+    val title: String,
 
-)
+    @ColumnInfo(name = "image")
+    @SerializedName("Images")
+    val image: ArrayList<String>,
+
+    @ColumnInfo(name = "rate")
+    @SerializedName("imdbRating")
+    val rate: String,
+
+    @ColumnInfo(name = "plot")
+    @SerializedName("Plot")
+    val plot: String,
+
+    @ColumnInfo(name = "type")
+    @SerializedName("Type")
+    val type: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var primaryKey: Int = 0
+}
+
+
